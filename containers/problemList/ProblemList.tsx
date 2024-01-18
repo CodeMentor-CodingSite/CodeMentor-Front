1073749135204de5f8874848edffbb93290797ab
ijaesin <ijaesinii@gmail.com>
2024-01-18 16:08:23 +0900

    feat: 문제 리스트 페이지네이션 UI 추가
    페이지네이션 기능은 추후 개발 예정
    
    Resolves: #4

--- containers/problemList/ProblemList.tsx
+++ containers/problemList/ProblemList.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import { Button } from '@nextui-org/react';
+import { Button, Pagination } from '@nextui-org/react';
 import {
   IconAttempted,
   IconSolved,
@@ -101,89 +101,104 @@ export default function ProblemList() {
   //   : { backgroundColor: '#ffffff' };
 
   return (
-    <table className="w-full text-xl table-fixed">
-      <colgroup>
-        <col width={8} />
-        <col width={54} />
-        <col width={10} />
-        <col width={16} />
-        <col width={12} />
-      </colgroup>
-      <thead>
-        <tr className="font-medium text-subtext text-left h-16 border-b dark:border-borderDark leading-[64px] ">
-          <th className="pl-3">Status</th>
-          <th className="flex items-center justify-between mr-10">
-            Title
-            <Button isIconOnly className="bg-white dark:bg-backgroundDark">
-              <IconDescendingOrder />
-            </Button>
-          </th>
-          <th>Solution</th>
-          <th className="relative">
-            Acceptance
-            <Button
-              isIconOnly
-              className="absolute bg-white right-3 dark:bg-backgroundDark top-3"
-            >
-              <IconDescendingOrder />
-            </Button>
-          </th>
-          <th className="relative">
-            Difficulty
-            <Button
-              isIconOnly
-              className="absolute right-0 bg-white dark:bg-backgroundDark top-3"
-            >
-              <IconDescendingOrder />
-            </Button>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {problemItems.map(
-          ({ status, num, title, solution, acceptance, difficulty }, index) => (
-            <tr
-              key={num}
-              className={`${index % 2 === 0 ? bgEven : bgOdd} h-16 leading-16`}
-            >
-              <td className="pl-5">
-                {/* eslint-disable-next-line no-nested-ternary */}
-                {status === 'solved' ? (
-                  <IconSolved />
-                ) : status === 'attempted' ? (
-                  <IconAttempted />
-                ) : null}
-              </td>
-
-              <td className="pl-3 truncate">
-                <Link
-                  href={`/problems/${num}`}
-                >{`${num.toString()}. ${title}`}</Link>
-              </td>
-
-              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-              <td className="pl-3">
-                <Link href={solution}>
-                  <IconSolution />
-                </Link>
-              </td>
-              <td className="pl-3">{`${acceptance}%`}</td>
-              <td
-                className={`${
-                  // eslint-disable-next-line no-nested-ternary
-                  difficulty === 'easy'
-                    ? 'text-themeGreen'
-                    : difficulty === 'hard'
-                      ? 'text-themeRed'
-                      : 'text-themeYellow'
-                } pl-3`}
+    <div>
+      <table className="w-full text-xl table-fixed">
+        <colgroup>
+          <col width={8} />
+          <col width={54} />
+          <col width={10} />
+          <col width={16} />
+          <col width={12} />
+        </colgroup>
+        <thead>
+          <tr className="font-medium text-subtext text-left h-16 border-b dark:border-borderDark leading-[64px] ">
+            <th className="pl-3">Status</th>
+            <th className="flex items-center justify-between mr-10">
+              Title
+              <Button isIconOnly className="bg-white dark:bg-backgroundDark">
+                <IconDescendingOrder />
+              </Button>
+            </th>
+            <th>Solution</th>
+            <th className="relative">
+              Acceptance
+              <Button
+                isIconOnly
+                className="absolute bg-white right-3 dark:bg-backgroundDark top-3"
               >
-                {capitalizeFirstChar(difficulty)}
-              </td>
-            </tr>
-          )
-        )}
-      </tbody>
-    </table>
+                <IconDescendingOrder />
+              </Button>
+            </th>
+            <th className="relative">
+              Difficulty
+              <Button
+                isIconOnly
+                className="absolute right-0 bg-white dark:bg-backgroundDark top-3"
+              >
+                <IconDescendingOrder />
+              </Button>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {problemItems.map(
+            (
+              { status, num, title, solution, acceptance, difficulty },
+              index
+            ) => (
+              <tr
+                key={num}
+                className={`${
+                  index % 2 === 0 ? bgEven : bgOdd
+                } h-16 leading-16`}
+              >
+                <td className="pl-5">
+                  {/* eslint-disable-next-line no-nested-ternary */}
+                  {status === 'solved' ? (
+                    <IconSolved />
+                  ) : status === 'attempted' ? (
+                    <IconAttempted />
+                  ) : null}
+                </td>
+
+                <td className="pl-3 truncate">
+                  <Link
+                    href={`/problems/${num}`}
+                  >{`${num.toString()}. ${title}`}</Link>
+                </td>
+
+                {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+                <td className="pl-3">
+                  <Link href={solution}>
+                    <IconSolution />
+                  </Link>
+                </td>
+                <td className="pl-3">{`${acceptance}%`}</td>
+                <td
+                  className={`${
+                    // eslint-disable-next-line no-nested-ternary
+                    difficulty === 'easy'
+                      ? 'text-themeGreen'
+                      : difficulty === 'hard'
+                        ? 'text-themeRed'
+                        : 'text-themeYellow'
+                  } pl-3`}
+                >
+                  {capitalizeFirstChar(difficulty)}
+                </td>
+              </tr>
+            )
+          )}
+        </tbody>
+      </table>
+
+      <Pagination
+        showControls
+        size="lg"
+        total={5}
+        initialPage={1}
+        className="float-right mt-8"
+      />
+    </div>
   );
 }
