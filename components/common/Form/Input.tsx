7f59f5712e94d3b130be797e7b10667905af58f2
ijaesin <ijaesinii@gmail.com>
2024-01-18 16:28:18 +0900

    fix: merge conflicts 해결

--- components/common/Form/Input.tsx
+++ components/common/Form/Input.tsx
@@ -1,37 +1,41 @@
 'use client';
 
-import type {InputHTMLAttributes} from "react";
+import type { InputHTMLAttributes } from 'react';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-    label?: string;
-    errorMessage?: string;
+  label?: string;
+  errorMessage?: string;
 }
 
 function classNames(...classes: string[]) {
-    return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(' ');
 }
 
-function Input({id, label = '', disabled = false, errorMessage = '', ...props}: InputProps) {
-    return (
-        <div className={classNames(disabled ? 'opacity-50 pointer-events-none' : '', 'relative mobile:text-sm')}>
-            {
-                (label !== "") && (
-                    <label htmlFor={id} className="text-gray-700">
-                        {label}
-                    </label>
-                )
-            }
-            <input
-                id={id}
-                type="text"
-                {...props}
-            />
-            {
-                errorMessage !== '' &&
-                <p className='text-red-500 text-md'>{errorMessage}</p>
-            }
-        </div>
-    );
+function Input({
+  id,
+  label = '',
+  disabled = false,
+  errorMessage = '',
+  ...props
+}: InputProps) {
+  return (
+    <div
+      className={classNames(
+        disabled ? 'opacity-50 pointer-events-none' : '',
+        'relative mobile:text-sm'
+      )}
+    >
+      {label !== '' && (
+        <label htmlFor={id} className="text-gray-700">
+          {label}
+        </label>
+      )}
+      <input id={id} type="text" {...props} />
+      {errorMessage !== '' && (
+        <p className="text-red-500 text-md">{errorMessage}</p>
+      )}
+    </div>
+  );
 }
 
 export default Input;
