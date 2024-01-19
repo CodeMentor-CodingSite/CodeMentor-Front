a8cd6eec70caf8ac9429dc84d275f26d6c0b23d8
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-01-19 14:31:06 +0900

    refactor: globals.css의 input스타일 tailwindcss class로 분리
    Related to: #6

--- app/userBoard/page.tsx
+++ app/userBoard/page.tsx
@@ -4,8 +4,7 @@ import {Languages, Skills, Solved, Submission, UserProfile} from "@/containers/u
 function UserBoardPage() {
     return (
         <section className='w-full h-full flex items-center space-x-[20px]'>
-            <section className='min-w-[276px] px-[16px] pt-[21px] pb-[100px] rounded-md
-             shadow-[0_0px_8px_1px_rgba(0,0,0,.18)] dark:bg-componentDark'>
+            <section className='flex flex-col space-y-8 w-[450px] px-[16px] pt-[21px] pb-[100px] userBoardSection'>
                 <UserProfile/>
                 <Languages/>
                 <Skills/>
